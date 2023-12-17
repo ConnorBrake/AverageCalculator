d0475faf40c83909cdc08f559dc40908b4f16a3c
Connor Brake <153332456+ConnorBrake@users.noreply.github.com>
2023-12-17 12:52:04 -0500

    Initial commit

---
+++ src/main/java/com/mycompany/averagecalculator/AverageCalculator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.averagecalculator;
+
+/**  
+ *
+ * @author 1337
+ */
+public class AverageCalculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
